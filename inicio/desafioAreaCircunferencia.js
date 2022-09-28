4f85304fc8e1705ef5560f1429acb592ec68af3b
kevifjq <kevifjq@gmail.com>
2022-09-28 00:18:05 -0300

    Desafio 1

---
+++ inicio/desafioAreaCircunferencia.js
@@ -0,0 +1,5 @@
+const PI = 3.1415926535898;
+let raio = 8;
+let area = PI * Math.pow(raio, 2);
+
+console.log(area.toFixed(2));
--- inicio/primeiro.js
+++
@@ -1,5 +0,0 @@
-console.log("Teste");
-
-for(let i = 5; i <28;i++){
-    console.log(i);
-}
